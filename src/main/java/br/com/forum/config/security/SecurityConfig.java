b10c2a816593c9c73c80b6b8d9490aaa14c48d34
savio.lopes <savio.lopes@cidadania.gov.br>
2022-06-09 01:25:44 -0300

    add auth basic

--- src/main/java/br/com/forum/config/security/SecurityConfig.java
+++ src/main/java/br/com/forum/config/security/SecurityConfig.java
@@ -1,24 +1,33 @@
 package br.com.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	
+	@Autowired
+	private AuthService authService;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
+		auth.userDetailsService(authService).passwordEncoder(new BCryptPasswordEncoder());	
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll();
+		http.authorizeRequests()
+		.antMatchers(HttpMethod.GET, "/topicos").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
 	}
 	
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$12$oFYXoJKyx3x1loV8dJOJNurtz0Tmi6JvwY97BvBlVK.iPpnHVArSS');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
